bae1309db7fa73c8b06e34238d92440635a02d17
Er-Saurabh-Gondane <saurabhgondane358@gmail.com>
2026-02-09 10:19:40 +0530

    delete edit remove functionality added successfully

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function ToDoItem() {
+
+function ToDoItem({ text, deleteTodo, id, toggle, isComplete, startEdit }) {
   return (
-    <div>ToDoItem</div>
-  )
+    <div className="flex items-center my-3 gap-2">
+      <div
+        onClick={() => toggle(id)}
+        className="flex flex-1 cursor-pointer items-center "
+      >
+        <div className="w-6 h-6 rounded-full bg-transparent flex items-center justify-center border border-green-700">
+          {isComplete ? (
+            <i className="fa-solid fa-check text-green-500 m-3 "></i>
+          ) : (
+            <div></div>
+          )}
+        </div>
+
+        <p
+          className={`text-slate-700 text-xl decoration-slate-500 ml-4 ${isComplete ? "line-through" : ""}`}
+        >
+          {text}
+        </p>
+      </div>
+      <div className="flex items-center gap-2">
+        <i
+          onClick={() => startEdit(id, text)}
+          className="fa-solid fa-pen text-blue-800 cursor-pointer"
+        ></i>
+        <i
+          className="fa-solid fa-trash text-red-600 cursor-pointer"
+          onClick={() => deleteTodo(id)}
+        ></i>
+      </div>
+    </div>
+  );
 }
 
-export default ToDoItem
+export default ToDoItem;
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import todo_icon from "../assets/todo_icon.png";
+import ToDoItem from "./ToDoItem";
 
-function ToDoList() {
+const ToDoList = ({ todoList, setToDoList }) => {
+
+  const inputRef = useRef();
+  const [editId, setEditId] = useState(null);
+
+  const addTask = () => {
+    const task = inputRef.current.value.trim();
+    if (task === "") return;
+
+    if (editId !== null) {
+      setToDoList((prev) =>
+        prev.map((todo) =>
+          todo.id === editId ? { ...todo, text: task } : todo
+        )
+      );
+      setEditId(null);
+    } else {
+      const newTodo = {
+        id: Date.now(),
+        text: task,
+        isComplete: false,
+      };
+
+      setToDoList((prev) => [...prev, newTodo]);
+    }
+
+    inputRef.current.value = "";
+  };
+
+  const startEdit = (id, text) => {
+    inputRef.current.value = text;
+    setEditId(id);
+  };
+
+  const deleteTodo = (id) => {
+    setToDoList((prev) =>
+      prev.filter((todo) => todo.id !== id)
+    );
+  };
+
+  const toggle = (id) => {
+    setToDoList((prev) =>
+      prev.map((todo) =>
+        todo.id === id
+          ? { ...todo, isComplete: !todo.isComplete }
+          : todo
+      )
+    );
+  };
+
   return (
-    <div>ToDoList</div>
-  )
-}
+    <div className="bg-white place-self-center w-11/12 max-w-md mt-20 flex flex-col p-7 rounded-xl">
 
-export default ToDoList
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src={todo_icon} alt="" />
+        <h1 className="text-3xl font-semibold">To Do List</h1>
+      </div>
+
+      {/* Input */}
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6"
+          placeholder="Add your task"
+        />
+
+        <button
+          onClick={addTask}
+          className="rounded-full bg-violet-600 text-white w-32 h-14"
+        >
+          {editId ? "UPDATE" : "ADD +"}
+        </button>
+      </div>
+
+      {/* List */}
+      <div>
+        {todoList.map((item) => (
+          <ToDoItem
+            key={item.id}
+            {...item}
+            deleteTodo={deleteTodo}
+            toggle={toggle}
+            startEdit={startEdit}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default ToDoList;
