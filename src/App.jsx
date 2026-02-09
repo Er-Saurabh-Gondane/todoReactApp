b3fef99dce6af09f6caedd298bccc01daa0e5a0a
Er-Saurabh-Gondane <saurabhgondane358@gmail.com>
2026-02-09 10:07:05 +0530

    header added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ToDoList from "./components/ToDoList";
+import Header from './components/Header'
 
 function App() {
+
+  const [todoList, setToDoList] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+
+  // Save to localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
+
   return (
-    <div className='text-4xl text-red-500'>App</div>
-  )
+    <div className="bg-stone-900 h-screen">
+      <Header/>
+      <ToDoList todoList={todoList} setToDoList={setToDoList} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="max-w-md mx-auto text-center pt-8">
+      <h1 className="text-4xl font-bold text-white mb-2">📝 To Do List App</h1>
+
+      <p className="text-gray-200 text-lg">Manage your daily tasks</p>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
